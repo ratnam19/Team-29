5c5d06ef358932d50b5985ebcaaab9ffdee5208c
Ratna Mandava <ratna.manognya019@gmail.com>
2019-10-27 15:42:24 -0400

    certifications by member & members by certification

--- app/public/api/reports/index.php
+++ app/public/api/reports/index.php
@@ -5,13 +5,11 @@ $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
 $stmt = $db->prepare(
-  'SELECT p.personId, p.firstName,p.lastName,p.gender,p.dob, p.email,p.mobilePhone,p.workPhone,p.street,p.city,p.zip,p.state,p.region,p.radioNumber,p.stationNumber,p.startDate,p.position,p.isActive,p.isEmployee,c.certificationId, c.certificationName, c.certifyingAgency, c.expiryPeriod, s.startDate as issuedDate, s.expirationDate
-FROM
-    CertificationStatus s
-INNER JOIN
-    Certification c ON c.certificationId = s.certificationId
-INNER JOIN
-    Person p ON p.personId = s.personId'
+  'SELECT p.personId, p.firstName,p.lastName,p.gender,p.dob, p.email,p.mobilePhone,p.workPhone,p.street,p.city,p.zip,p.state,p.region,p.radioNumber,p.stationNumber,p.startDate,p.position,p.isActive,p.isEmployee,
+  c.certificationId, c.certificationName, c.certifyingAgency, c.expiryPeriod,s.startDate as issuedDate, s.expirationDate
+FROM CertificationStatus s
+INNER JOIN Certification c ON c.certificationId = s.certificationId
+INNER JOIN  Person p ON p.personId = s.personId'
 );
 $stmt->execute();
 $persons = $stmt->fetchAll();
--- app/public/js/membercombo.app.js
+++ app/public/js/membercombo.app.js
@@ -2,18 +2,33 @@ var memberCombinationApp = new Vue({
   el: '#memberCombinationApp',
   data: {
     membercombination: [],
+    persons: [],
+    certifications: [],
     filter: {
-    certificationName: ''
-  }
+    certificationName: '',
+    personId: ''
+   }
   },
   methods: {
     fetchmemberCombination() {
       fetch('api/reports/index.php')
       .then(response => response.json())
       .then(json => { memberCombinationApp.membercombination = json })
+    },
+    fetchMembers() {
+      fetch('api/reports/fetchPersons.php')
+      .then(response => response.json())
+      .then(json => { memberCombinationApp.persons = json })
+    },
+    fetchCertifications() {
+      fetch('api/reports/fetchCertis.php')
+      .then(response => response.json())
+      .then(json => { memberCombinationApp.certifications = json })
     }
   },
   created() {
     this.fetchmemberCombination();
+    this.fetchMembers();
+    this.fetchCertifications();
   }
 });
